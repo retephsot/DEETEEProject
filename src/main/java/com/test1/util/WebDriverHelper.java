d69ffea6392a32077b9fb710f9677d03c7968d0e
retephsot <synhlee@yahoo.com>
2016-06-15 21:43:09 -0400

    Added DownLoad Issues File test

--- src/main/java/com/test1/util/WebDriverHelper.java
+++ src/main/java/com/test1/util/WebDriverHelper.java
@@ -2,13 +2,17 @@ package com.test1.util;
 
 import java.awt.Toolkit;
 import java.security.InvalidParameterException;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverHelper 
 {
@@ -22,9 +26,40 @@ public class WebDriverHelper
 		}
 		else if(browser.equalsIgnoreCase("Chrome"))
 		{
+			//original setup for google chrome
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\seleniumJar\\chromedriver_win32\\chromedriver.exe");
+			
+			//to enable download using chrome
+//			System.setProperty("webdriver.chrome.driver", System.getProperty("C:\\seleniumJar\\chromedriver_win32")
+//					+ System.getProperty("file.separator")
+//					+ "BrowserDrivers"
+//					+ System.getProperty("file.separator")
+//					+ "chromedriver.exe");
+//			String downloadFilepath = "C:\\testfolder\\SeleniumDownloads";
+//
+//			HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+//			chromePrefs.put("profile.default_content_settings.popups", 0);
+//			chromePrefs.put("download.default_directory", downloadFilepath);
+//			chromePrefs.put("pdfjs.disabled", true);
+//
+//			//Save Chrome Opions
+//			ChromeOptions options = new ChromeOptions();
+//			HashMap<String, Object> chromeOptionsMap = new HashMap<String, Object>();
+//			options.setExperimentalOption("prefs", chromePrefs);
+//			options.addArguments("--test-type");
+//
+//
+//			DesiredCapabilities cap = DesiredCapabilities.chrome();
+//			cap.setCapability(ChromeOptions.CAPABILITY, chromeOptionsMap);
+//			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+//			cap.setCapability(ChromeOptions.CAPABILITY, options); //to enable download using chrome
+
+			//original chrome setup
 			driver = new ChromeDriver();
+			
+			//driver = new ChromeDriver(cap);
+			
 		}
 		else if(browser.equalsIgnoreCase("IE"))
 		{
--- src/test/java/com/test1/tests/TestSuccessfulDLIssuesFile.java
+++ src/test/java/com/test1/tests/TestSuccessfulDLIssuesFile.java
@@ -9,6 +9,8 @@ import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import com.test1.pages.LogInPage;
 import org.testng.annotations.BeforeMethod;
+
+import java.awt.AWTException;
 import java.awt.Toolkit;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -18,24 +20,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 
-public class TestSuccessfulLogIn extends TestBase
+public class TestSuccessfulDLIssuesFile extends TestBase
 
 {
 
   @Test (dataProvider = "dataProvider")
-  public void testSuccessfulLogIn(String username, String password) 
-				  throws InterruptedException, IOException 
+  public void testSuccessfulIssuesFileDownLoad(String username, String password, String downloadPath, 
+		  String filename) throws InterruptedException, IOException, AWTException 
   {
 	
-	//The entry point LogInPage object below can now be removed because its added to TestBase can now inherit this
-    //LogInPage logInPage = new LogInPage(driver);
 	  
 	boolean testResult = loginpage.login(username, password)
-								  .isLogInSuccessful(username); 
+								  .DownloadIssuesList()
+								  .isFileDownLoadSuccessful(downloadPath, filename); 
   
 	 System.out.println(testResult);	
 	 
-	 Assert.assertTrue(testResult, "Login is not successful for username " + username );
+	 Assert.assertTrue(testResult, "The Issues File download was not successful for " + username );
 	 
   }
   
