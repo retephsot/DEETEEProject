7c94eedca23ea1b729a6525e97b8012c84dfc0ed
retephsot <synhlee@yahoo.com>
2016-06-28 14:29:58 -0400

    Adding TestSuccessfulAddAttachment DB check

--- src/main/java/com/test1/pages/PageBase.java
+++ src/main/java/com/test1/pages/PageBase.java
@@ -112,6 +112,70 @@ public class PageBase
 	}
 	
 	
+	public boolean isAddAttachmentSuccessfulCheckDB (String filetitle, String filename) throws InterruptedException, ClassNotFoundException, SQLException
+	{
+		
+		boolean testresults;
+		
+		String query1 = "";
+		
+		// Connection url
+		String dbUrl = "jdbc:sqlserver://dts.cfm3qnzohy8k.us-east-1.rds.amazonaws.com:1433;databaseName=DTS_QA";
+											
+		// DB username
+		String username = "dtsapp";
+								
+		// DB password
+		String password = "WfL65GrBVZYjGgXI8AQ8";
+		
+		//String ColName = "FileTitle";
+		
+		// SQL query
+		//String query = "SELECT * FROM ccDaily.ScheduledTests WHERE RequestedOn LIKE \"2016-03-22%\";"; //for mysql		
+		//String query = "SELECT UserName FROM ccDemo.Users WHERE UserFirstName = \"Rick\";  // for mysql
+		String query = "SELECT FileName FROM DTS_QA.dbo.Attachment WHERE FileTitle = '" + filetitle + "';";  //for sqlserver
+		
+		//Local SQL Server JDBC driver
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				
+		// get connection to DB
+		Connection con = DriverManager.getConnection(dbUrl, username, password);
+		
+		// to show if DB connection is successful
+		System.out.println("Connection Successfull");
+        System.out.println(con); 
+		
+		// Create statement object
+		Statement stmt = con.createStatement();
+		
+		// Send SQL query to DB
+		ResultSet rs = stmt.executeQuery(query);
+				
+		// While loop to get ResultSet all rows of data
+		while(rs.next())
+		{			
+			query1 = rs.getString(1);
+		}
+						
+		// Close DB connection
+		con.close();
+		
+		System.out.println("The value for UserName is " + query1);
+			
+		
+		testresults = query1.equals(filename);;
+		
+        Thread.sleep(1000);
+		
+		System.out.println(testresults);
+				
+		return testresults;
+	}
+	
+	
+	
+	
+	
 	
 
 }
