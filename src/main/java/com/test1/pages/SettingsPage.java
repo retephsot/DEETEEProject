2ac0af87936706ca6c701fbdc092c979aa87be8c
retephsot <synhlee@yahoo.com>
2016-07-08 14:20:56 -0400

    updated Rtest and created newAdminCreateTest

---
+++ src/main/java/com/test1/pages/SettingsPage.java
@@ -0,0 +1,12 @@
+package com.test1.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SettingsPage extends PageBase{
+	
+	public SettingsPage (WebDriver driver)
+	{
+		super(driver);
+	}
+
+}
